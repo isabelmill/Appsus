7c149680809856d0a47d140f348968b634aec1bc
Isabel Mill <isabelmavd2@gmail.com>
2022-03-02 14:53:41 +0200

    changes

--- js/apps/keep/pages/note-index.cmp.js
+++ js/apps/keep/pages/note-index.cmp.js
@@ -2,6 +2,23 @@ export default {
     // props: [""],
     template: `
         <section>
+            <header>
+            <div class="keep-footer-layout">
+            <input class="search-bar" type="text">
+            </div>
+            </header>
+
+            <main class="keep-main-layout">
+            <div class="keep-side-bar-layout">
+             <ul>
+              <li>puki</li>
+              <li>puki</li>
+              <li>puki</li>
+             </ul>
+            </div>
+            <input class="keep-input" type="text">
+            </main>
+
         </section>
     `,
     components: {},
